fc530cb8bee1a5a3af90b0d6a3486f91755a5605
Tharindu Deshan Ruhunage <deshan.r@fidenz.com>
2017-05-14 18:34:09 +0530

    added challenges 81-88

---
+++ batch commands/heroku_push_configs.bat
@@ -0,0 +1,20 @@
+cmd /c  heroku config:set COOKIE_SECRET=COOKIE_SECRET
+cmd /c  heroku config:set FACEBOOK_ID=FACEBOOK_ID
+cmd /c  heroku config:set FACEBOOK_SECRET=FACEBOOK_SECRET
+cmd /c  heroku config:set GITHUB_ID=GITHUB_ID
+cmd /c  heroku config:set GITHUB_SECRET=GITHUB_SECRET
+cmd /c  heroku config:set GOOGLE_ID=GOOGLE_ID
+cmd /c  heroku config:set LINKEDIN_ID=LINKEDIN_ID
+cmd /c  heroku config:set LINKEDIN_SECRET=LINKEDIN_SECRET
+cmd /c  heroku config:set MONGOHQ_URL=mongodb://heroku_jcfs8h33:qqlmui03rkdton2iqmh2ucgpgu@ds137141.mlab.com:37141/heroku_jcfs8h33
+cmd /c  heroku config:set NODE_VERBOSE=true
+cmd /c  heroku config:set NPM_CONFIG_LOGLEVEL=error
+cmd /c  heroku config:set PEER=PEER
+cmd /c  heroku config:set SESSION_SECRET=SESSION_SECRET
+cmd /c  heroku config:set TWITTER_KEY=TWITTER_KEY
+cmd /c  heroku config:set TWITTER_SECRET=TWITTER_SECRET
+cmd /c  heroku config:set TWITTER_TOKEN=TWITTER_TOKEN
+cmd /c  heroku config:set TWITTER_TOKEN_SECRET=TWITTER_TOKEN_SECRET
+@echo off
+echo Press enter to exit
+set /p input=
---
+++ batch commands/reinitdb.bat
@@ -0,0 +1,5 @@
+cmd /c  cd ..
+cmd /c  node seed
+@echo off
+echo Press enter to exit
+set /p input=
